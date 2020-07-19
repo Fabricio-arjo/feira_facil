35c2634b24acab28c6445ba118715f5e3c1a3482
Fabricio <faabricio.araujo@gmail.com>
2020-07-19 09:10:06 -0300

    SplashScreen

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -25,11 +25,31 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
        body: Container(
-          color: Colors.purple,
+
+         decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage('images/splash.png'),
+              fit: BoxFit.cover,
+          ),
+        ),
+          
+          /* color: Colors.purple,
           padding: EdgeInsets.all(60),
           child: Center(
-            child: Image.asset("images/logo.png"),
-          ), 
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                 Image.asset("images/logo.png"),
+                 Text("Gasto", style: TextStyle(
+                     color: Colors.yellow
+                 ),
+                 textAlign: TextAlign.right,
+                )
+              ],
+            ),
+            //child: Image.asset("images/logo.png"),
+            
+          ),*/ 
        ),   
     );
   }
