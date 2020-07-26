a56873957425e55345bd89d01ba3ef27d9207548
Fabricio <faabricio.araujo@gmail.com>
2020-07-26 11:16:45 -0300

    Splash Screen

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'Home.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -8,49 +9,61 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
- @override
+  @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    Timer(Duration(seconds: 2),(){
-       Navigator.pushReplacement(
-         context, 
-         MaterialPageRoute(builder: (_)=> Home())
-         );
+    Timer(Duration(seconds: 6), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (_) => Home()));
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Container(
-
-         decoration: new BoxDecoration(
+      body: Container(
+        /*decoration: new BoxDecoration(
             image: new DecorationImage(
               image: new AssetImage('images/splash.png'),
               fit: BoxFit.cover,
           ),
+        ),*/
+
+        color: Colors.purple,
+        padding: EdgeInsets.all(60),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                "images/logo.png",
+                height: 171,
+                width: 171,
+              ),
+              SizedBox(
+                width: 500,
+                child: TextLiquidFill(
+                  text: 'Gasto Certo',
+                  waveColor: Colors.white,
+                  boxBackgroundColor: Colors.purple,
+                  textStyle: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  boxHeight: 50.0,
+                ),
+              ),
+
+              /*Text(
+                "\nGasto Certo",
+                style: TextStyle(color: Colors.yellow),
+                textAlign: TextAlign.right,
+              )*/
+            ],
+          ),
+          //child: Image.asset("images/logo.png"),
         ),
-          
-          /* color: Colors.purple,
-          padding: EdgeInsets.all(60),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                 Image.asset("images/logo.png"),
-                 Text("Gasto", style: TextStyle(
-                     color: Colors.yellow
-                 ),
-                 textAlign: TextAlign.right,
-                )
-              ],
-            ),
-            //child: Image.asset("images/logo.png"),
-            
-          ),*/ 
-       ),   
+      ),
     );
   }
 }
